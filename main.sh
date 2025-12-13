e5064d783292b2a77f9bba169f2bedad0a2d1fe2
a <160086103+randomeagler@users.noreply.github.com>
2025-12-13 13:02:01 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo starting...
 cd auth
-java -Xms128M -Xmx510M -jar paper.jar &
+java -Xms128M -Xmx510M -jar paper.jar & sleep 1m &
 zrok reserve private 0.0.0.0:25565 --backend-mode tcpTunnel --unique-name auth
