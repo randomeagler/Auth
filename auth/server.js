249d69cb1da718f6eb6f5bb6c2f4b4c88d9b5953
a <160086103+randomeagler@users.noreply.github.com>
2025-12-12 18:47:51 -0600

    Add basic Minecraft server setup with ScriptServer

---
+++ auth/server.js
@@ -0,0 +1,20 @@
+const ScriptServer = require('scriptserver');
+
+const server = new ScriptServer({
+  core: {
+    jar: 'paper.jar', // Name of your server JAR file
+    args: ['-Xmx2G', '-Xms1G'], // Arguments for RAM usage
+    rcon: {
+      port: '25565',
+      password: 'root'
+    }
+  }
+});
+
+server.start();
+console.log('Minecraft server starting...');
+
+// Example of sending a command after server starts (requires rcon setup)
+server.on('started', () => {
+  server.send('say Node.js server is now running!');
+});
